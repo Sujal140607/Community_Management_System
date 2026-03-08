8e8f7cbb2d5d4547bb966215048635f84a2e5d9a
Sujal140607 <skp140607@gmail.com>
2026-03-08 14:55:42 +0530

    first

---
+++ Server/src/models/area.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const areaSchema = new mongoose.Schema({
+    areaName: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    district: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    pincode: {
+        type: Number,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        
+    }
+})
